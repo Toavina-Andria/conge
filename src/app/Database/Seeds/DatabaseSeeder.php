8d84d5ca4bbce7448223efcf0f35d706b0d4046c
Toavina <teoandriamonta@gmail.com>
2026-05-13 08:42:52 +0300

    Modification des seeder

--- src/app/Database/Seeds/DatabaseSeeder.php
+++ src/app/Database/Seeds/DatabaseSeeder.php
@@ -8,9 +8,22 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        $this->truncateAllTables();
+
         $this->call('App\Database\Seeds\DepartementSeeder');
         $this->call('App\Database\Seeds\EmployeSeeder');
         $this->call('App\Database\Seeds\TypeCongeSeeder');
         $this->call('App\Database\Seeds\SoldeSeeder');
+    }
+
+    private function truncateAllTables(): void
+    {
+        $this->db->query('SET FOREIGN_KEY_CHECKS = 0');
+        $this->db->table('soldes')->truncate();
+        $this->db->table('conges')->truncate();
+        $this->db->table('types_conge')->truncate();
+        $this->db->table('employes')->truncate();
+        $this->db->table('departements')->truncate();
+        $this->db->query('SET FOREIGN_KEY_CHECKS = 1');
     }
 }
